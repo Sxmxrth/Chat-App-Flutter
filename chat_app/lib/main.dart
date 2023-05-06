edb105cfde64c7457f5e34a9e0097e26b573ae4e
Sxmxrth <samarthgupta1202@gmail.com>
2023-05-06 21:09:10 +0530

    initial commit

---
+++ chat_app/lib/main.dart
@@ -0,0 +1,27 @@
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, unused_import
+
+import 'package:flutter/material.dart';
+import 'package:chat_app/screens/welcome_screen.dart';
+import 'package:chat_app/screens/login_screen.dart';
+import 'package:chat_app/screens/registration_screen.dart';
+import 'package:chat_app/screens/chat_screen.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          bodyLarge: TextStyle(color: Colors.black54),
+        ),
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
